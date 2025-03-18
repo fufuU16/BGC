56aa745b9c5aba433b149248ef5f83519b22904b
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2025-03-18 14:54:03 +0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,16 +1,16 @@
 <?php
-// Database configuration
-$host = 'bgcdb.mysql.database.azure.com'; // Azure database host
-$username = 'judymalahay'; // Azure database username
-$password = 'Malahayj123'; // Your Azure database password
-$database = 'bgc_database'; // Your Azure database name
+$servername = "bgcdb.mysql.database.azure.com";
+$username = "judymalahay";
+$password = "Malahayj123";
+$database = "bgc_database";
 $port = 3306;
-// Create a connection
-$conn = new mysqli($host, $username, $password, $database);
 
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, "/certs/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 
+if (!mysqli_real_connect($conn, $servername, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
+} 
+
+echo "Connected successfully with SSL";
 ?>
