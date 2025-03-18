58883dea5f5e468254432a79f18bff7b694fc946
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2025-03-18 14:45:29 +0800

    Update Routedetail.php

--- Routedetail.php
+++ Routedetail.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include 'db_connection.php'; // Include the database connection
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/error_log.txt');
