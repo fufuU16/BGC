1bec77aef31c63e4d14cbb90ca5b3f44c09588fe
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2025-03-18 14:56:44 +0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -6,7 +6,7 @@ $database = "bgc_database";
 $port = 3306;
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, "/certs/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn, NULL, NULL, "/home/site/wwwroot/certs/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 
 if (!mysqli_real_connect($conn, $servername, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("Connection failed: " . mysqli_connect_error());
